38423d5be110172bb47fdf1abb17eba5d007cfc0
dudska12 <skadud0113@naver.com>
2025-09-18 06:18:06 +0900

    feat: [공통컴포넌트] 태그 init #1

--- src/components/ui/Tag.tsx
+++ src/components/ui/Tag.tsx
@@ -1,12 +1,24 @@
+import SmallMoveIcon from "@/assets/icon/SmallMoveIcon.svg";
+/**
+ * Tag 컴포넌트
+ *
+ * @param type - 태그의 종류 (예: "primary", "secondary")
+ * @param size - 태그의 크기 (예: "sm", "md", "lg")
+ *
+ * @example
+ * <Tag type="primary" size="md" />
+ */
 interface TagPros {
   type: string;
   size: string;
+  content: string;
 }
 
-export default function Tag({ type }: TagPros) {
+export default function Tag({ type, size, content }: TagPros) {
   return (
     <div>
-      <p></p>
+      <img></img>
+      <p>{content}</p>
     </div>
   );
 }
