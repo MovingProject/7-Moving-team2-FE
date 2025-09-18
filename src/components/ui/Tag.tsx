345f4ebe2c711026787e66703c3f40c671f8b854
dudska12 <skadud0113@naver.com>
2025-09-18 16:31:41 +0900

    [공통컴포넌트] 태그-기초구조작성 #1

--- src/components/ui/Tag.tsx
+++ src/components/ui/Tag.tsx
@@ -1,23 +1,54 @@
 import SmallMoveIcon from "@/assets/icon/SmallMoveIcon.svg";
-/**
- * Tag 컴포넌트
- *
- * @param type - 태그의 종류 (예: "primary", "secondary")
- * @param size - 태그의 크기 (예: "sm", "md", "lg")
- *
- * @example
- * <Tag type="primary" size="md" />
- */
-interface TagPros {
-  type: string;
-  size: string;
-  content: string;
+import SmallMoveIconSm from "@/assets/icon/SmallMoveIcon-1.svg";
+import HomeMoveIcon from "@/assets/icon/HomeMoveIcon.svg";
+import HomeMoveIconSm from "@/assets/icon/HomeMoveIcon-1.svg";
+import OfficeMoveIcon from "@/assets/icon/OfficeMoveIcon.svg";
+import OfficeMoveIconSm from "@/assets/icon/OfficeMoveIcon-1.svg";
+
+type IconType = "smallMove" | "homeMove" | "officeMove" | "default";
+type IconSize = "default" | "sm";
+type BoxType = "default" | "radius";
+
+interface Icon {
+  src: string;
+  width: number;
+  height: number;
 }
 
-export default function Tag({ type, size, content }: TagPros) {
+interface TagProps {
+  type: IconType;
+  size: IconSize;
+  content: string;
+  borderType?: BoxType;
+}
+
+const iconMap: Record<Exclude<IconType, "default">, Record<IconSize, Icon>> = {
+  smallMove: {
+    default: SmallMoveIcon,
+    sm: SmallMoveIconSm,
+  },
+  homeMove: {
+    default: HomeMoveIcon,
+    sm: HomeMoveIconSm,
+  },
+  officeMove: {
+    default: OfficeMoveIcon,
+    sm: OfficeMoveIconSm,
+  },
+};
+
+export default function Tag({ type, size, content }: TagProps) {
+  const iconSrc = type === "default" ? null : iconMap[type]?.[size] || iconMap[type]?.default;
+
+  //TODO: borderType가 default일때 or radius일때 최상위 container가 바뀌는 조건만들어서 적용할것.
+
   return (
-    <div>
-      <img></img>
+    <div className="flex gap-5 border border-red-500">
+      {iconSrc ? (
+        <img src={iconSrc.src} width={iconSrc.width} height={iconSrc.height} alt={`${type} icon`} />
+      ) : (
+        <div></div>
+      )}
       <p>{content}</p>
     </div>
   );
