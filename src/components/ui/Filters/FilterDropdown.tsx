75109ced0ec9da41193bbbc23dc6100f74cf6a61
KimJesung <andrewkim99@naver.com>
2025-09-18 13:37:00 +0900

    feat: dropdown 컴포넌트 DropdownList 추가 및 필터 파일 수정 #1

--- src/components/ui/Filters/FilterDropdown.tsx
+++ src/components/ui/Filters/FilterDropdown.tsx
@@ -1,13 +1,15 @@
 "use client";
 
 import { useState } from "react";
+import DropdownList from "./DropdownList";
 
 type FilterDropdownProps = {
   label: string;
   options: string[];
   selected?: string;
   onChangeAction: (value: string) => void;
-  size?: "sm" | "md" | "lg";
+  buttonSize?: "sm" | "md" | "lg"; // 버튼 사이즈
+  dropdownSize?: "sm" | "md" | "lg"; // dropdown 사이즈
   variant?: "default" | "active";
 };
 
@@ -16,13 +18,14 @@ export default function FilterDropdown({
   options,
   selected,
   onChangeAction,
-  size = "sm",
-  variant = "default",
+  buttonSize = "sm", // 패딩 기준
+  dropdownSize = "sm",
+  variant = "default", // 버튼 활성화 여부
 }: FilterDropdownProps) {
   const [isOpen, setIsOpen] = useState(false);
   const isActive = isOpen || variant === "active";
 
-  const sizeClasses = {
+  const buttonSizeClasses = {
     sm: "py-6 text-sm",
     md: "py-8 px-10 text-base",
     lg: "py-16 px-24 text-lg",
@@ -30,31 +33,25 @@ export default function FilterDropdown({
 
   const baseClasses = "border rounded-[8px] transition-colors duration-150";
   const defaultClasses = "bg-[#FFF] text-[#1F1F1F]";
-  const activeClasses = "bg-[#F5FAFF] border-[1px] border-solid border-[#1B92FF]";
+  const activeClasses = "bg-[#F5FAFF] border-[1px] border-solid border-[#1B92FF] text-[#1B92FF]";
 
   return (
     <div>
       <button
         onClick={() => setIsOpen((prev) => !prev)}
-        className={`${baseClasses} ${sizeClasses[size]} ${isActive ? activeClasses : defaultClasses}`}
+        className={`${baseClasses} ${buttonSizeClasses[buttonSize]} ${isActive ? activeClasses : defaultClasses}`}
       >
         {selected || label}
       </button>
       {isOpen && (
-        <ul className="absolute z-10 mt-2 w-40 rounded-lg border bg-white shadow-lg">
-          {options.map((option) => (
-            <li
-              key={option}
-              onClick={() => {
-                onChangeAction(option);
-                setIsOpen(false);
-              }}
-              className="cursor-pointer px-4 py-2 hover:bg-gray-100"
-            >
-              {option}
-            </li>
-          ))}
-        </ul>
+        <DropdownList
+          options={options}
+          onSelectAction={(value) => {
+            onChangeAction(value);
+            setIsOpen(false);
+          }}
+          size={dropdownSize}
+        />
       )}
     </div>
   );
