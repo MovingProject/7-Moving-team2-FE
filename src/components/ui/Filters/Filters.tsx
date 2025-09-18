88168506e82327960f9a3389ab98d1cba74b6b1d
KimJesung <andrewkim99@naver.com>
2025-09-18 15:19:21 +0900

    refactor: Filters.tsx 코드 중복 개선 리팩토링 #1

--- src/components/ui/Filters/Filters.tsx
+++ src/components/ui/Filters/Filters.tsx
@@ -7,81 +7,56 @@ type FilterProps = {
   onChangeAction: (value: string) => void;
 };
 
-export function RegionFilter(props: FilterProps) {
-  return (
-    <FilterDropdown
-      label="지역"
-      options={[
-        "전체",
-        "서울",
-        "경기",
-        "인천",
-        "강원",
-        "충북",
-        "충남",
-        "세종",
-        "대전",
-        "전북",
-        "전남",
-        "광주",
-        "경북",
-        "경남",
-        "대구",
-        "울산",
-        "부산",
-        "제주",
-      ]}
-      buttonSize="sm"
-      dropdownSize="sm"
-      variant="default"
-      className=""
-      dropdownClassName="h-179 overflow-y-auto grid grid-cols-2"
-      {...props}
-    />
-  );
+function createFilter(label: string, options: string[], dropdownClassName: string = "") {
+  return function CustomFilter(props: FilterProps) {
+    return (
+      <FilterDropdown
+        label={label}
+        options={options}
+        buttonSize="sm"
+        dropdownSize="sm"
+        variant="default"
+        className=""
+        dropdownClassName={dropdownClassName}
+        {...props}
+      />
+    );
+  };
 }
 
-export function ServiceFilter(props: FilterProps) {
-  return (
-    <FilterDropdown
-      label="서비스"
-      options={["전체", "소형이사", "가정이사", "사무실이사"]}
-      buttonSize="sm"
-      dropdownSize="sm"
-      variant="default"
-      className=""
-      dropdownClassName=""
-      {...props}
-    />
-  );
-}
+// 필터 종류
+export const RegionFilter = createFilter(
+  "지역",
+  [
+    "전체",
+    "서울",
+    "경기",
+    "인천",
+    "강원",
+    "충북",
+    "충남",
+    "세종",
+    "대전",
+    "전북",
+    "전남",
+    "광주",
+    "경북",
+    "경남",
+    "대구",
+    "울산",
+    "부산",
+    "제주",
+  ],
+  "h-179 overflow-y-auto grid grid-cols-2"
+);
 
-export function SortFilter(props: FilterProps) {
-  return (
-    <FilterDropdown
-      label="정렬"
-      options={["리뷰 많은 순", "평점 높은 순", "경력 높은 순", "확정 많은 순"]}
-      buttonSize="sm"
-      dropdownSize="sm"
-      variant="default"
-      className=""
-      dropdownClassName=""
-      {...props}
-    />
-  );
-}
+export const ServiceFilter = createFilter("서비스", ["전체", "소형이사", "가정이사", "사무실이사"]);
 
-export function SortFilterFast(props: FilterProps) {
-  return (
-    <FilterDropdown
-      label="정렬"
-      options={["이사 빠른 순", "요청일 빠른 순"]}
-      buttonSize="sm"
-      dropdownSize="sm"
-      variant="default"
-      className=""
-      dropdownClassName=""
-      {...props}
-    />
-  );
-}
+export const SortFilter = createFilter("정렬", [
+  "리뷰 많은 순",
+  "평점 높은 순",
+  "경력 높은 순",
+  "확정 많은 순",
+]);
+
+export const SortFilterFast = createFilter("정렬", ["이사 빠른 순", "요청일 빠른 순"]);
