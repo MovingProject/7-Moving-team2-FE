8ec77e7f0c01bc2ffc7d20c7ad26e3dd17636b98
KimJesung <andrewkim99@naver.com>
2025-09-20 15:12:24 +0900

    feat: 기사 페이지 용 정렬 필터 추가 #1

--- src/components/ui/Filters/Dropdown.tsx
+++ src/components/ui/Filters/Dropdown.tsx
@@ -2,11 +2,11 @@
 
 import clsx from "clsx";
 
-type DropdownType = "filter" | "sort" | "profile" | "notification"; // 드롭다운 용도
+type DropdownType = "filter" | "sort" | "profile" | "notification"; // 드롭다운이 사용되는 요소들
 type DropdownLayout = "default" | "grid"; // 옵션 배치 방식
 type DropdownSize = "sm" | "md"; // 드롭다운의 사이즈
 type DropdownRadius = "xl" | "xxl" | "xxxl"; // border-radius
-type DropdownScroll = "none" | "scrollable"; // 스크롤 여부
+type DropdownScroll = "none" | "scrollable"; // 드롭다운 내부 스크롤 여부
 
 interface DropdownProps {
   type: DropdownType;
@@ -17,7 +17,7 @@ interface DropdownProps {
   options: string[];
   onSelect: (value: string) => void;
   header?: React.ReactNode; // profile, notification 전용
-  footer?: React.ReactNode; // profile 전용
+  footer?: React.ReactNode; // profile 전용 (로그아웃)
 }
 // 드롭다운 타입 별 사이즈
 const typeSizeMap: Record<DropdownType, Record<DropdownSize, string>> = {
@@ -26,8 +26,8 @@ const typeSizeMap: Record<DropdownType, Record<DropdownSize, string>> = {
     md: "w-[328px] py-[16px] px-[24px] text-[18px]", // 큰 버전
   },
   sort: {
-    sm: "w-[105px] py-[6px] pl-[8px] pr-[6px] text-[12px]", // 정렬 작은 드롭다운
-    md: "w-[120px] py-[8px] px-[10px] text-[14px]", // 정렬 큰 드롭다운
+    sm: "w-[115px] py-[6px] pl-[8px] pr-[6px] text-[12px]", // 정렬 필터 작은 드롭다운
+    md: "w-[135px] py-[8px] px-[10px] text-[14px]", // 정렬 필터 큰 드롭다운
   },
   profile: {
     sm: "w-[150px] pt-[10px] pb-[6px] px-[10px] text-[14px]", // 프로필 메뉴 작은 버전
@@ -84,7 +84,7 @@ export function Dropdown({
             key={opt}
             className={clsx(
               "cursor-pointer px-4 py-2 hover:bg-gray-100",
-              type === "notification" && idx !== 0 && "border-t border-[#E5E7EB]", // 알림일 때만 위에 border
+              type === "notification" && idx !== 0 && "border-t border-[#E5E7EB]", // 알림일 때만 border-top
               layout === "grid" && idx % 2 === 1 && "border-l border-[#E5E7EB]"
             )}
             onClick={() => onSelect(opt)}
