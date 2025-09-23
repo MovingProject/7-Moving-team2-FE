e7c54b2560b545faeada45575d96fa0a8ff199a2
dudska12 <skadud0113@naver.com>
2025-09-23 13:45:27 +0900

    feat:[페이지]-로그인 UI구현 #2

---
+++ src/app/login/components/LoginForm.tsx
@@ -0,0 +1,35 @@
+import Input from "@/components/ui/Input";
+import { useState } from "react";
+import Button from "@/components/ui/Button";
+export default function LoginForm() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  //TODO : FIX : INPUT에서 바꿔야할거생김 <input tpye:{} /> 이부분 조절할수있도록해야됨.
+  return (
+    <div>
+      <form className="flex flex-col gap-3.5">
+        <label>이메일</label>
+        <Input
+          type="basic"
+          size="full"
+          errorPosition="right"
+          error="잘못된 이메일 형식입니다."
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        ></Input>
+        <label className="mt-[8px]">비밀번호</label>
+        <Input
+          type="basic"
+          size="full"
+          errorPosition="right"
+          error="잘못된 비밀번호입니다."
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        ></Input>
+        <div className="m-4" />
+        <Button>로그인</Button>
+      </form>
+    </div>
+  );
+}
