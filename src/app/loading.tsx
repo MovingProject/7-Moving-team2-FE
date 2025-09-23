79a92c67bb0790a0a351615e0000ed9efdf9b9a1
aha-hyeong <ahahyeong@gmail.com>
2025-09-23 09:22:38 +0900

    feat/로딩 페이지 구현 완료 #1

---
+++ src/app/loading.tsx
@@ -0,0 +1,9 @@
+import LogoSpinner from "@/components/ui/LogoSpinner";
+
+export default function Loading() {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <LogoSpinner />
+    </div>
+  );
+}
