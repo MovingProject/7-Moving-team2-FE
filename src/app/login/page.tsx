cf529e4344fb64fb7ad93031355f2e7af8887bc7
dudska12 <skadud0113@naver.com>
2025-09-23 17:43:36 +0900

    feat:[페이지]-로그인 UI 구현 완료 #2

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,19 +2,26 @@
 import LoginForm from "@/app/login/components/LoginForm";
 import SocialLogin from "./components/SocialLogin";
 import Link from "next/link";
+import SwitchButton from "./components/SwitchButton";
+import LogoText from "@/assets/icon/Logo-2.svg";
 
 export default function Login() {
-  return (
-    <div className="mt-10 flex items-center justify-center">
-      <div className="flex flex-col items-center">
-        <LoginForm></LoginForm>
-        <div className="mx-auto mt-6 flex max-w-160 flex-row items-center gap-2">
-          <p> 아직 무빙 회원이 아니신가요? </p>
-          <Link href="/signup" className="text-primary">
-            이메일로 회원가입하기
-          </Link>
-        </div>
+  const pretendardXs = "text-[#6B6B6B] font-pretendard text-[16px] font-normal leading-[18px]";
+  const pretendardXsUnderline =
+    "text-[var(--Primary-blue-300,#1B92FF)] font-pretendard text-[16px] font-semibold leading-[20px] underline";
 
+  return (
+    <div className="mt-10 flex justify-center">
+      <div className="flex w-full max-w-[480px] flex-col items-center">
+        <div className="mb-[40px] flex flex-col items-center">
+          <img className="mb-[24px]" src={LogoText.src}></img>
+          <SwitchButton />
+        </div>
+        <LoginForm></LoginForm>
+        <p className={`mt-[24px] ${pretendardXs}`}>
+          아직 무빙 회원이 아니신가요?{" "}
+          <a className={pretendardXsUnderline}>이메일로 회원가입하기</a>
+        </p>
         <SocialLogin></SocialLogin>
       </div>
     </div>
