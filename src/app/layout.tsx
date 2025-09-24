b0ba1ee5d734a827b130abe43d4e3d9809dc5c5e
dudska12 <skadud0113@naver.com>
2025-09-24 14:48:26 +0900

    chore: 오타수정 및 Nav 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "../styles/globals.css";
+import Nav from "@/components/ui/nav/nav";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -13,7 +14,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
-      <body>{children}</body>
+      <body>
+        <Nav />
+        {children}
+      </body>
     </html>
   );
 }
