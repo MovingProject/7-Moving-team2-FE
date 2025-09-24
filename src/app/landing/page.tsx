e55e051101a06894af27970fe49d38dd934c3cdf
dudska12 <skadud0113@naver.com>
2025-09-24 14:17:59 +0900

    feat:[페이지]-Landing UI 구현완료 #4

---
+++ src/app/landing/page.tsx
@@ -0,0 +1,52 @@
+import MovingSmall from "@/assets/img/Landing1.svg";
+import MovingHome from "@/assets/img/Landing2.svg";
+import MovingBusiness from "@/assets/img/Landing3.svg";
+import Button from "@/components/ui/Button";
+import aaa from "@/assets/img/Landing_md_01.svg";
+import bbb from "@/assets/img/Landing_md_02.svg";
+import ccc from "@/assets/img/Landing_md_03.svg";
+
+export default function Landing() {
+  return (
+    <div className="bg-[#F4F7FB]">
+      <div className="lg flex flex-col items-center">
+        <p className="pt-32 pb-8 text-center font-[Pretendard] text-[24px] leading-[50px] font-semibold text-black lg:text-[36px]">
+          원하는 이사 서비를 요청하고 <br />
+          견적을 받아보세요.
+        </p>
+        <div className="flex flex-col lg:h-[600px] lg:flex-row lg:items-stretch lg:gap-0">
+          {/* 왼쪽 */}
+          <img src={MovingSmall.src} className="lg:hidden" />
+          <img
+            src={aaa.src}
+            className="hidden lg:block lg:h-[508px] lg:w-[432px] lg:object-contain"
+          />
+
+          {/* 오른쪽 */}
+          <div className="flex flex-col lg:h-[508px] lg:w-[604px] lg:gap-0">
+            <img src={MovingHome.src} className="lg:hidden" />
+            <img
+              src={bbb.src}
+              className="hidden lg:block lg:h-[287px] lg:w-[764px] lg:object-contain"
+            />
+
+            <img src={MovingBusiness.src} className="lg:hidden" />
+            <img
+              src={ccc.src}
+              className="hidden lg:block lg:h-[287px] lg:w-[764px] lg:object-contain"
+            />
+          </div>
+        </div>
+
+        <div className="mt-8 flex w-full flex-col items-center gap-4 pb-10 lg:flex-row lg:justify-center lg:gap-6">
+          <Button className="w-[320px]" radius="full">
+            로그인
+          </Button>
+          <Button className="w-[320px]" variant="secondary" radius="full">
+            회원가입
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+}
