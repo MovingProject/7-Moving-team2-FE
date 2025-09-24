330569026997bbe347efcab2d3d0cd1c2fbb35a9
dudska12 <skadud0113@naver.com>
2025-09-24 16:33:15 +0900

    fix:[페이지]- 랜딩  이미지 클릭 시 포커스 아웃라인 제거 #4

--- src/app/landing/page.tsx
+++ src/app/landing/page.tsx
@@ -2,36 +2,37 @@ import MovingSmall from "@/assets/img/Landing1.svg";
 import MovingHome from "@/assets/img/Landing2.svg";
 import MovingBusiness from "@/assets/img/Landing3.svg";
 import Button from "@/components/ui/Button";
-import aaa from "@/assets/img/Landing_md_01.svg";
-import bbb from "@/assets/img/Landing_md_02.svg";
-import ccc from "@/assets/img/Landing_md_03.svg";
+import MovingSmallMd from "@/assets/img/Landing_md_01.svg";
+import MovingHomeMd from "@/assets/img/Landing_md_02.svg";
+import MovingBusinessMd from "@/assets/img/Landing_md_03.svg";
 
 export default function Landing() {
   return (
-    <div className="h-full bg-[#F4F7FB] lg:h-full">
-      <div className="flex flex-col items-center lg:h-screen lg:justify-between">
-        <p className="pt-32 pb-8 text-center font-[Pretendard] text-[24px] leading-[50px] font-semibold text-black lg:text-[36px]">
+    <div className="min-h-screen bg-[#F4F7FB] outline-none">
+      <div className="flex min-h-screen flex-col items-center justify-between caret-transparent outline-none focus:ring-0">
+        <p className="pt-32 pb-8 text-center font-[Pretendard] text-[24px] leading-[50px] font-semibold text-black outline-none lg:text-[36px]">
           원하는 이사 서비스를 요청하고 <br />
           견적을 받아보세요.
         </p>
-        <div className="flex flex-col lg:h-[600px] lg:flex-row lg:items-stretch lg:gap-0">
-          <img src={MovingSmall.src} className="lg:hidden" />
+
+        <div className="flex flex-col outline-none lg:h-[600px] lg:flex-row lg:items-stretch lg:gap-0">
+          <img src={MovingSmall.src} className="cursor-pointer outline-none lg:hidden" />
           <img
-            src={aaa.src}
-            className="hidden lg:block lg:h-[508px] lg:w-[432px] lg:object-contain"
+            src={MovingSmallMd.src}
+            className="hidden cursor-pointer outline-none lg:block lg:h-[508px] lg:w-[432px] lg:object-contain"
           />
 
-          <div className="flex flex-col lg:h-[508px] lg:w-[604px] lg:gap-0">
-            <img src={MovingHome.src} className="lg:hidden" />
+          <div className="flex flex-col outline-none lg:h-[508px] lg:w-[604px] lg:gap-0">
+            <img src={MovingHome.src} className="cursor-pointer outline-none lg:hidden" />
             <img
-              src={bbb.src}
-              className="hidden lg:block lg:h-[287px] lg:w-[764px] lg:object-contain"
+              src={MovingHomeMd.src}
+              className="hidden cursor-pointer outline-none lg:block lg:h-[287px] lg:w-[764px] lg:object-contain"
             />
 
-            <img src={MovingBusiness.src} className="lg:hidden" />
+            <img src={MovingBusiness.src} className="cursor-pointer outline-none lg:hidden" />
             <img
-              src={ccc.src}
-              className="hidden lg:block lg:h-[287px] lg:w-[764px] lg:object-contain"
+              src={MovingBusinessMd.src}
+              className="hidden cursor-pointer outline-none lg:block lg:h-[287px] lg:w-[764px] lg:object-contain"
             />
           </div>
         </div>
