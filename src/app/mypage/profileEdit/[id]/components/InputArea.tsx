4b58cad933b76564191e570a968125ce36c14a8e
dudska12 <skadud0113@naver.com>
2025-09-25 18:35:48 +0900

    임시중단 mypage/profileEdit/[id]/page.tsx 맨밑에 못하는이유적어놓았음

--- src/app/mypage/profileEdit/[id]/components/InputArea.tsx
+++ src/app/mypage/profileEdit/[id]/components/InputArea.tsx
@@ -1,14 +1,16 @@
 import Input from "@/components/ui/Input";
+import { InputType } from "@/components/ui/Input";
 
 interface InputAreaProps {
   label: string;
+  type?: InputType;
 }
 
-export default function InputArea({ label }: InputAreaProps) {
+export default function InputArea({ label, type = "basic" }: InputAreaProps) {
   return (
     <>
       <label className="">{label}</label>
-      <Input className="bg-[#F7F7F7]" type="basic" value="a" onChange={() => {}}></Input>
+      <Input className="bg-[#F7F7F7]" type={type} value="a" onChange={() => {}}></Input>
     </>
   );
 }
