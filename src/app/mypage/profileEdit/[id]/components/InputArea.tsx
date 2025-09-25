07cad5fe3b8520d45b8958bf809249fdb34c909f
dudska12 <skadud0113@naver.com>
2025-09-25 11:21:53 +0900

    feat:[페이지]-마이페이지-프로필수정/기사님- input 컴포init #5

---
+++ src/app/mypage/profileEdit/[id]/components/InputArea.tsx
@@ -0,0 +1,10 @@
+import Input from "@/components/ui/Input";
+
+export default function InputArea() {
+  return (
+    <>
+      <label></label>
+      <Input className="bg-[#F7F7F7]" type="basic" value="a" onChange={() => {}}></Input>
+    </>
+  );
+}
