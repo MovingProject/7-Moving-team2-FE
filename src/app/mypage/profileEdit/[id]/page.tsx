b7d51cd9ab217160e1cd3da9fefee26d4e0a9672
dudska12 <skadud0113@naver.com>
2025-09-25 10:39:00 +0900

    feat:[페이지]-마이페이지-프로필수정페이지-init #5

---
+++ src/app/mypage/profileEdit/[id]/page.tsx
@@ -0,0 +1,12 @@
+export default function Edit() {
+  return (
+    <div>
+      <p>title:타이틀수정</p>
+      <div>
+        <div>인풋폼</div>
+        <div>태그폼?</div>
+      </div>
+      <div>버튼</div>
+    </div>
+  );
+}
