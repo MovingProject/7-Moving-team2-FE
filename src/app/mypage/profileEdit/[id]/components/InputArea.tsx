7332633db8e9a4fc11e969ab7a5c8d6c440213ed
dudska12 <skadud0113@naver.com>
2025-09-25 11:41:43 +0900

    refactor: input컴포 Label을 합쳐서 새로운 InputArea컴포 생성 #5

--- src/app/mypage/profileEdit/[id]/components/InputArea.tsx
+++ src/app/mypage/profileEdit/[id]/components/InputArea.tsx
@@ -1,9 +1,13 @@
 import Input from "@/components/ui/Input";
 
-export default function InputArea() {
+interface InputAreaProps {
+  label: string;
+}
+
+export default function InputArea({ label }: InputAreaProps) {
   return (
     <>
-      <label></label>
+      <label className="">{label}</label>
       <Input className="bg-[#F7F7F7]" type="basic" value="a" onChange={() => {}}></Input>
     </>
   );
