cc2f9f5dcae2133785d1dfbb05d4c14e5f384958
dudska12 <skadud0113@naver.com>
2025-09-25 18:16:25 +0900

    feat:[페이지]-마이페이지-프로필수정-태그폼컴포 생성 #5

--- src/app/mypage/profileEdit/[id]/page.tsx
+++ src/app/mypage/profileEdit/[id]/page.tsx
@@ -3,8 +3,28 @@ import Input from "@/components/ui/Input";
 import InputArea from "./components/InputArea";
 import upload from "@/assets/img/upload.svg";
 import ImageInputArea from "./components/ImageInputArea";
+import TagForm from "./components/TagForm";
 
 export default function Edit() {
+  const regions = [
+    "서울",
+    "경기",
+    "인천",
+    "강원",
+    "충북",
+    "충남",
+    "세종",
+    "대전",
+    "전북",
+    "전남",
+    "광주",
+    "경북",
+    "경남",
+    "대구",
+    "울산",
+    "부산",
+    "제주",
+  ];
   return (
     <div>
       <p className="border-b border-gray-500 pb-4 text-[18px] leading-[26px] font-bold text-[#1F1F1F]">
@@ -12,15 +32,20 @@ export default function Edit() {
       </p>
       <div>
         <div>
-          <form>
-            <InputArea label="별명" />
-            {/* TODO : 일단 틀만구현하고 style은 추후에 컴포넌트수정후 변경하셈 */}
-            <ImageInputArea />
-            <InputArea label="경력" />
-            <InputArea label="한 줄 소개" />
-          </form>
+          <InputArea label="별명" />
+          {/* TODO : 일단 틀만구현하고 style은 추후에 컴포넌트수정후 변경하셈 */}
+          <ImageInputArea />
+          <InputArea label="경력" />
+          <InputArea label="한 줄 소개" />
         </div>
-        <div>태그폼?</div>
+        <div>
+          <TagForm
+            Tags={["소형이사", "가정이사", "사무실이사"]}
+            label="상세설명"
+            colType="flex"
+          ></TagForm>
+          <TagForm Tags={regions} label="가능구역" colType="grid"></TagForm>
+        </div>
       </div>
       <div>버튼</div>
     </div>
