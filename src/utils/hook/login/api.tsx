a102110c700204f76b52496718b41820a09eab60
dudska12 <skadud0113@naver.com>
2025-09-26 15:26:25 +0900

    feat: useLogin Hook 생성 #35

---
+++ src/utils/hook/login/api.tsx
@@ -0,0 +1,41 @@
+import axios from "axios";
+import { useMutation } from "@tanstack/react-query";
+
+const host = "http://localhost:4000";
+const api = axios.create({
+  baseURL: host,
+  headers: { "Content-Type": "application/json" },
+});
+
+type roleType = "CONSUMER" | "DRIVER";
+interface LoginDTO {
+  email: string;
+  password: string;
+  role: roleType;
+}
+
+interface UserData {
+  id: string;
+  email: string;
+  name: string;
+  role: roleType;
+  createdAt: string;
+  isProfileRegistered: boolean;
+}
+
+interface LoginResponse {
+  success: boolean;
+  data: UserData;
+}
+
+export function useLogin() {
+  return useMutation<LoginResponse, Error, LoginDTO>({
+    mutationFn: async (data: LoginDTO) => {
+      const res = await api.post<LoginResponse>("/auth/signin", data);
+      return res.data;
+    },
+    onError: (error) => {
+      // TODO : ERROR 모달로 이동해서 로그인실패했습니다 ㄱ
+    },
+  });
+}
