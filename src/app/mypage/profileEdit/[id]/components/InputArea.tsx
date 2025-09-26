02aedc4bf45a45e468df4d8c05466c28353ba7af
dudska12 <skadud0113@naver.com>
2025-09-26 13:08:27 +0900

    fix:레이아웃 수정 및 태그폼 이중Dom구조로 수정 #29

--- src/app/mypage/profileEdit/[id]/components/InputArea.tsx
+++ src/app/mypage/profileEdit/[id]/components/InputArea.tsx
@@ -1,16 +1,21 @@
 import Input from "@/components/ui/Input";
 import { InputType } from "@/components/ui/Input";
+import clsx from "clsx";
 
 interface InputAreaProps {
   label: string;
   type?: InputType;
+  borderOption?: "not";
 }
 
-export default function InputArea({ label, type = "basic" }: InputAreaProps) {
+export default function InputArea({ label, type = "basic", borderOption }: InputAreaProps) {
+  const containerClass = clsx("mt-4 pb-8", borderOption !== "not" && "border-b border-gray-500");
   return (
-    <>
-      <label className="">{label}</label>
-      <Input className="bg-[#F7F7F7]" type={type} value="a" onChange={() => {}}></Input>
-    </>
+    <div className={containerClass}>
+      <label className="font-Pretendard text-[16px] leading-[26px] font-semibold text-[var(--Black-Black-300,#373737)]">
+        {label}
+      </label>
+      <Input className="bg-[#f1efef]" type={type} value="a" onChange={() => {}}></Input>
+    </div>
   );
 }
