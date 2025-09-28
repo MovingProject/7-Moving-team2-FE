362b103d1b4cdd25776a9c1dc94f8ddfbee536ba
dudska12 <skadud0113@naver.com>
2025-09-28 18:56:31 +0900

    feat: 로그인 후 새로고침해도 사용자 캐시 유지 #43

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { PersistQueryClientProvider } from "@tanstack/react-query-persist-client";
 import { createAsyncStoragePersister } from "@tanstack/query-async-storage-persister";
+import { ReactNode, useState, useEffect } from "react";
 
 const queryClient = new QueryClient();
 
@@ -14,13 +15,28 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const [persister, setPersister] = useState<any>(null);
+  useEffect(() => {
+    const asyncPersister = createAsyncStoragePersister({
+      storage: window.localStorage,
+    });
+    setPersister(asyncPersister);
+  }, []);
   return (
     <html lang="ko">
       <body>
         <QueryClientProvider client={queryClient}>
           <Nav />
-          {children}
-          {process.env.NODE_ENV === "development" && <ReactQueryDevtools initialIsOpen={false} />}
+          {persister ? (
+            <PersistQueryClientProvider client={queryClient} persistOptions={{ persister }}>
+              {children}
+              {process.env.NODE_ENV === "development" && (
+                <ReactQueryDevtools initialIsOpen={false} />
+              )}
+            </PersistQueryClientProvider>
+          ) : (
+            children
+          )}
         </QueryClientProvider>
       </body>
     </html>
