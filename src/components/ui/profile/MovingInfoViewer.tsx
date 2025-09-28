a436a5a0e260f0a362c075e0e9a5c807bfbfc026
dslee0525 <lds8835@gmail.com>
2025-09-29 01:18:43 +0900

    feat:카드 컴포넌트 관련 데이터 타입 선언한 내용 적용 및 확인 #1

--- src/components/ui/profile/MovingInfoViewer.tsx
+++ src/components/ui/profile/MovingInfoViewer.tsx
@@ -2,20 +2,20 @@
 import { useEffect, useState } from "react";
 import Image from "next/image";
 export interface MovingInfo {
-  services?: string[];
-  locals?: string[];
+  serviceTypes?: string[];
+  serviceAreas?: string[];
   reviewCount?: number;
   rating?: number;
-  career?: number;
-  deals?: number;
-  date?: string;
+  careerYears?: number;
+  confirmedCount?: number;
+  moveAt?: string;
   price?: number;
-  departure?: string;
-  destination?: string;
+  departureAddress?: string;
+  arrivalAddress?: string;
 }
 export interface MovingInfoViewerProps {
   info: MovingInfo;
-  infoType?: "localservice" | "review" | "estimate" | "route";
+  infoType?: "driverService" | "review" | "estimate" | "route";
 }
 
 export default function MovingInfoViewer({ info, infoType = "review" }: MovingInfoViewerProps) {
@@ -28,35 +28,27 @@ export default function MovingInfoViewer({ info, infoType = "review" }: MovingIn
     return () => window.removeEventListener("resize", handler);
   }, []);
   switch (infoType) {
-    case "localservice":
+    case "driverService":
       return (
         <div className="flex flex-col gap-x-4 gap-y-2 text-xs md:flex-row md:divide-x md:divide-gray-300 lg:gap-x-6 lg:text-base">
           <dl className="flex items-center gap-2 pr-2.5 lg:pr-6">
             <dt className="rounded border border-gray-300 bg-gray-100 px-2 py-0.5 text-gray-500">
               제공 서비스
             </dt>
-            <dd className="flex gap-1.5 text-gray-800">
-              {info.services?.map((service) => (
-                <span key={service}>{service}</span>
-              ))}
-            </dd>
+            <dd className="flex gap-1.5 text-gray-800">{info.serviceTypes?.join(", ")}</dd>
           </dl>
-          <dl className="flex gap-2">
+          <dl className="flex items-center gap-2">
             <dt className="rounded border border-gray-300 bg-gray-100 px-2 py-0.5 text-gray-500">
               지역
             </dt>
-            <dd className="flex gap-1.5 text-gray-800">
-              {info.locals?.map((local) => (
-                <span key={local}>{local}</span>
-              ))}
-            </dd>
+            <dd className="flex gap-1.5 text-gray-800">{info.serviceAreas?.join(", ")}</dd>
           </dl>
         </div>
       );
     case "review":
       return (
         <div className="flex items-center gap-2.5 text-xs lg:gap-4 lg:text-base">
-          <dl className="flex gap-2">
+          <dl className="flex items-center gap-2">
             <dt className="sr-only text-gray-500">별점</dt>
             <dd className="flex items-center gap-1 text-gray-800">
               <Image
@@ -69,16 +61,16 @@ export default function MovingInfoViewer({ info, infoType = "review" }: MovingIn
               <span className="text-gray-500">({info.reviewCount ?? 0})</span>
             </dd>
           </dl>
-          <dl className="flex gap-2">
+          <dl className="flex items-center gap-2">
             <dt className="text-gray-500">경력</dt>
             <dd className="flex gap-2 text-gray-800">
-              <span>{info.career ?? 0}년</span>
+              <span>{info.careerYears ?? 0}년</span>
             </dd>
           </dl>
-          <dl className="flex gap-2">
+          <dl className="flex items-center gap-2">
             <dt className="sr-only text-gray-500">거래</dt>
             <dd className="flex gap-1 text-gray-800">
-              <span className="flex gap-0.5">{info.deals ?? 0}건</span>
+              <span className="flex gap-0.5">{info.confirmedCount ?? 0}건</span>
               <span className="text-gray-500">확정</span>
             </dd>
           </dl>
@@ -90,7 +82,7 @@ export default function MovingInfoViewer({ info, infoType = "review" }: MovingIn
           <dl className="flex items-center gap-2 pr-2.5 lg:pr-6">
             <dt className="text-gray-500">이사일</dt>
             <dd className="flex gap-2 text-gray-800">
-              <span>{info.date ?? "-"}</span>
+              <span>{info.moveAt ?? "-"}</span>
             </dd>
           </dl>
           <dl className="flex items-center gap-2">
@@ -107,20 +99,20 @@ export default function MovingInfoViewer({ info, infoType = "review" }: MovingIn
           <dl className="flex items-center gap-2 lg:pr-6">
             <dt className="rounded bg-gray-100 px-2 py-0.5 text-gray-500">이사일</dt>
             <dd className="flex gap-2 text-gray-800">
-              <span>{info.date ?? "-"}</span>
+              <span>{info.moveAt ?? "-"}</span>
             </dd>
           </dl>
           <div className="flex items-center gap-2.5 divide-x divide-gray-300 lg:gap-6">
             <dl className="flex items-center gap-2 pr-2.5 lg:pr-6">
               <dt className="rounded bg-gray-100 px-2 py-0.5 text-gray-500">출발지</dt>
               <dd className="flex gap-2 text-gray-800">
-                <span>{info.departure ?? "-"}</span>
+                <span>{info.departureAddress ?? "-"}</span>
               </dd>
             </dl>
             <dl className="flex items-center gap-2">
               <dt className="rounded bg-gray-100 px-2 py-0.5 text-gray-500">도착지</dt>
               <dd className="flex gap-2 text-gray-800">
-                <span>{info.destination ?? "-"}</span>
+                <span>{info.arrivalAddress ?? "-"}</span>
               </dd>
             </dl>
           </div>
---
+++ src/components/ui/profile/UserProfileArea.tsx
@@ -0,0 +1,91 @@
+"use client";
+
+import { useMemo } from "react";
+import LikeButton from "../LikeButton";
+import MovingInfoViewer, { MovingInfo } from "./MovingInfoViewer";
+import ProfileViewer from "./ProfileViewer";
+import { AreaMap, AreaType } from "@/types/areaTypes";
+import { DriverProfileData, QuotationData, RequestData, UserData } from "@/types/card";
+import CardText from "../card/CardText";
+import { isDriverUser } from "@/utils/type-guards";
+
+interface UserProfileInfoProps {
+  user: UserData;
+  quotation?: QuotationData;
+  request?: RequestData;
+  show?: ("name" | "oneLiner" | "services" | "reviews" | "estimated" | "likes")[];
+  className?: string;
+}
+
+export default function UserProfileArea({
+  user,
+  request,
+  quotation,
+  show = ["name", "reviews"],
+  className,
+}: UserProfileInfoProps) {
+  if (!user) {
+    return null;
+  }
+  const isVisible = (key: "name" | "oneLiner" | "services" | "reviews" | "estimated" | "likes") =>
+    show.includes(key);
+
+  const isDriver = isDriverUser(user);
+
+  const profile = user.profile; // DriverProfileData | ConsumerProfileData
+  const driverProfile = isDriver ? (profile as DriverProfileData) : null;
+  const movingInfo: MovingInfo = useMemo(() => {
+    const price = quotation?.price;
+    const moveAt = request?.moveAt;
+
+    const info: MovingInfo = {
+      reviewCount: driverProfile?.reviewCount,
+      rating: driverProfile?.rating,
+      careerYears: driverProfile?.careerYears,
+      confirmedCount: driverProfile?.confirmedCount,
+      serviceAreas: driverProfile?.driverServiceAreas?.map(
+        (areaKey) => AreaMap[areaKey as AreaType]
+      ),
+      price: price,
+      moveAt: moveAt,
+    };
+
+    return info;
+  }, [driverProfile, quotation?.price, request?.moveAt]);
+
+  return (
+    <div className={`relative flex items-center gap-3 ${className ?? ""}`}>
+      {driverProfile?.image && <ProfileViewer initialImageUrl={driverProfile.image} />}
+
+      <div className="flex flex-col gap-1 lg:gap-2">
+        {isVisible("name") && (
+          <CardText className="text-base font-semibold text-gray-800 lg:text-lg">
+            {user.name ?? "-"}
+          </CardText>
+        )}
+        {isVisible("oneLiner") && driverProfile?.oneLiner && (
+          <CardText className="text-sm text-gray-600 lg:text-base">
+            {driverProfile.oneLiner}
+          </CardText>
+        )}
+
+        {isVisible("reviews") && movingInfo && (
+          <MovingInfoViewer info={movingInfo} infoType="review" />
+        )}
+        {isVisible("services") && movingInfo && (
+          <MovingInfoViewer info={movingInfo} infoType="driverService" />
+        )}
+        {isVisible("estimated") && movingInfo && (
+          <MovingInfoViewer info={movingInfo} infoType="estimate" />
+        )}
+        {isVisible("likes") && driverProfile?.likes && (
+          <LikeButton
+            count={driverProfile?.likes.likedCount}
+            isLiked={driverProfile?.likes.isLikedByCurrentUser}
+            className="absolute top-2 right-3 lg:top-1/2 lg:right-4 lg:-translate-y-1/2"
+          />
+        )}
+      </div>
+    </div>
+  );
+}
