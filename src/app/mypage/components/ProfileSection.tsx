defdf26947e12221c4375ac0cbd0affa9771cbd7
KimJesung <andrewkim99@naver.com>
2025-09-29 23:00:35 +0900

    feat: ProfileSection에 프로필 카드 컴포넌트 추가 #5

--- src/app/mypage/components/ProfileSection.tsx
+++ src/app/mypage/components/ProfileSection.tsx
@@ -1,24 +1,32 @@
 "use client";
 
+import ProfileCard from "@/components/ui/card/ProfileCard";
+import { getRandomProfileImage } from "@/utils/constant/getProfileImage";
+import { UserData } from "@/types/card";
+
 export default function ProfileSection() {
-  // TODO: API 연동 시 유저 데이터 받아오기
-  const user = {
+  const user: UserData = {
+    userId: "user-driver-001",
     name: "김코드",
-    description: "고객님의 물품을 소중하고 안전하게 운송하여 드립니다.",
-    rating: 5.0,
-    reviewCount: 178,
-    career: "7년",
-    matched: 343,
-    services: ["포장이사", "소형이사", "가전이사"],
-    region: ["서울", "경기"],
+    role: "DRIVER",
+    profile: {
+      driverId: "drv-001",
+      nickname: "김코드 기사님",
+      oneLiner: "고객님의 물품을 소중하고 안전하게 운송하여 드립니다.",
+      image: getRandomProfileImage(),
+      reviewCount: 178,
+      rating: 5.0,
+      careerYears: 7,
+      confirmedCount: 343,
+      driverServiceTypes: ["SMALL_MOVE", "HOME_MOVE"],
+      driverServiceAreas: ["SEOUL", "GYEONGGI"],
+      likes: { likedCount: 20, isLikedByCurrentUser: false },
+    },
   };
 
   return (
-    <section className="rounded-2xl border-1 border-gray-200 bg-gray-50 p-6">
-      {/* 프로필 카드 */}
-      <div>
-        <p>카드 컴포넌트 import해서 쓸 예정</p>
-      </div>
+    <section>
+      <ProfileCard user={user} />
     </section>
   );
 }
