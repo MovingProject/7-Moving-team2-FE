78fa184f0ba15b740413f47e947c4585ae7f5596
KimJesung <andrewkim99@naver.com>
2025-09-29 10:23:28 +0900

    fix: 기존 img next image로 수정

--- src/components/ui/Filters/CheckFilter.tsx
+++ src/components/ui/Filters/CheckFilter.tsx
@@ -1,7 +1,6 @@
-/* eslint-disable @next/next/no-img-element */
-
 "use client";
 
+import Image from "next/image";
 import CheckBox from "@/assets/icon/CheckBox.svg";
 import CheckBoxEmpty from "@/assets/icon/CheckBoxEmpty.svg";
 
@@ -44,8 +43,8 @@ export default function CheckFilter({
                 }
                 className="peer hidden"
               />
-              <img src={CheckBoxEmpty.src} alt="unchecked" className="peer-checked:hidden" />
-              <img src={CheckBox.src} alt="checked" className="hidden peer-checked:block" />
+              <Image src={CheckBoxEmpty} alt="unchecked" className="peer-checked:hidden" />
+              <Image src={CheckBox} alt="checked" className="hidden peer-checked:block" />
               전체선택
             </label>
           )}
@@ -69,8 +68,8 @@ export default function CheckFilter({
                   }
                   className="peer hidden"
                 />
-                <img src={CheckBoxEmpty.src} alt="unchecked" className="peer-checked:hidden" />
-                <img src={CheckBox.src} alt="checked" className="hidden peer-checked:block" />
+                <Image src={CheckBoxEmpty} alt="unchecked" className="peer-checked:hidden" />
+                <Image src={CheckBox} alt="checked" className="hidden peer-checked:block" />
               </span>
             </label>
           </div>
@@ -88,8 +87,8 @@ export default function CheckFilter({
               onChange={() => onToggle(opt.value)}
               className="peer hidden"
             />
-            <img src={CheckBoxEmpty.src} alt="unchecked" className="peer-checked:hidden" />
-            <img src={CheckBox.src} alt="checked" className="hidden peer-checked:block" />
+            <Image src={CheckBoxEmpty} alt="unchecked" className="peer-checked:hidden" />
+            <Image src={CheckBox} alt="checked" className="hidden peer-checked:block" />
           </label>
         ))}
       </ul>
