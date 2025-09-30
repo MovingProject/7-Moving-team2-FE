e7c40e44f92f4654506a571304f05f5a2432f3a3
KimJesung <andrewkim99@naver.com>
2025-09-30 11:10:42 +0900

    feat: 마이페이지 반응형 추가 #5

--- src/app/mypage/components/Header.tsx
+++ src/app/mypage/components/Header.tsx
@@ -1,6 +1,6 @@
 export default function Header() {
   return (
-    <header className="flex w-full flex-col items-start bg-white py-8">
+    <header className="flex w-full flex-col items-start bg-white py-8 pl-5">
       <h1 className="text-2xl font-semibold text-gray-800">마이페이지</h1>
     </header>
   );
