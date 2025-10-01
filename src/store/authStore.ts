2e83f3e51380bac5767896ce9cc22c6647c3f6ad
KimJesung <andrewkim99@naver.com>
2025-10-01 17:03:20 +0900

    feat: authStore, apiClient 추가 및 기존 로그인 수정 #74

---
+++ src/store/authStore.ts
@@ -0,0 +1,25 @@
+import { create } from "zustand";
+
+export type RoleType = "CONSUMER" | "DRIVER";
+
+export interface User {
+  id: string;
+  email: string;
+  name: string;
+  role: string;
+  createdAt: string;
+  isProfileRegistered?: boolean;
+  profileId?: string;
+}
+
+interface AuthState {
+  user: User | null;
+  setUser: (user: User) => void;
+  clearUser: () => void;
+}
+
+export const useAuthStore = create<AuthState>((set) => ({
+  user: null,
+  setUser: (user) => set({ user }),
+  clearUser: () => set({ user: null }),
+}));
