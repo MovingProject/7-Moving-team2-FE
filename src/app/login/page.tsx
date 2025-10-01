eab0d941e1d1cb744c826e1d11435e104d6aff9d
KimJesung <andrewkim99@naver.com>
2025-10-01 17:03:20 +0900

    feat: authStore, apiClient 추가 및 기존 로그인 수정 #74

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -6,38 +6,27 @@ import LogoText from "@/assets/icon/Logo-2.svg";
 import Image from "next/image";
 import { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
-import { useQuery, useQueryClient } from "@tanstack/react-query";
+import { useAuthStore } from "@/store/authStore";
 
-type UserResponse = {
-  success: boolean;
-  data: {
-    id: string;
-    email: string;
-    name: string;
-    role: "DRIVER" | "CONSUMER";
-    createdAt: string;
-    isProfileRegistered: boolean;
-  };
-};
+// type UserResponse = {
+//   success: boolean;
+//   data: {
+//     id: string;
+//     email: string;
+//     name: string;
+//     role: "DRIVER" | "CONSUMER";
+//     createdAt: string;
+//     isProfileRegistered: boolean;
+//   };
+// };
 
 export default function Login() {
-  const pretendardXs = "text-[#6B6B6B] font-pretendard text-[16px] font-normal leading-[18px]";
-  const pretendardXsUnderline =
-    "text-[var(--Primary-blue-300,#1B92FF)] font-pretendard text-[16px] font-semibold leading-[20px] underline";
-
   const [role, setRole] = useState<"CONSUMER" | "DRIVER">("CONSUMER");
-
   const router = useRouter();
-  const queryClient = useQueryClient();
-
-  const { data: user } = useQuery<UserResponse | null, Error>({
-    queryKey: ["user"],
-    queryFn: () => queryClient.getQueryData<UserResponse>(["user"]) ?? null,
-    staleTime: Infinity,
-  });
+  const user = useAuthStore((s) => s.user);
 
   useEffect(() => {
-    if (user?.success) {
+    if (user) {
       router.replace("/landing");
     }
   }, [user, router]);
@@ -46,17 +35,17 @@ export default function Login() {
     <div className="mt-10 flex justify-center">
       <div className="flex w-full max-w-[480px] flex-col items-center">
         <div className="mb-[40px] flex flex-col items-center">
-          <Image className="mb-[24px]" src={LogoText.src} alt="" width={100} height={100}></Image>
+          <Image className="mb-[24px]" src={LogoText} alt="" width={100} height={100}></Image>
           <SwitchButton selected={role} setSelected={setRole} />
         </div>
-        <LoginForm role={role}></LoginForm>
-        <p className={`mt-[24px] ${pretendardXs}`}>
+        <LoginForm role={role} />
+        <p className="font-pretendard mt-[24px] text-[16px] leading-[18px] font-normal text-[#6B6B6B]">
           아직 무빙 회원이 아니신가요?{" "}
-          <a className={pretendardXsUnderline} href="/signUp">
+          <a className="font-pretendard text-[16px] leading-[20px] font-semibold text-[var(--Primary-blue-300,#1B92FF)] underline">
             이메일로 회원가입하기
           </a>
         </p>
-        <SocialLogin></SocialLogin>
+        <SocialLogin />
       </div>
     </div>
   );
