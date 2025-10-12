cc070a3a8c3c89f2d0d67ebe37327f0c3d16ae45
KimJesung <andrewkim99@naver.com>
2025-10-12 22:40:02 +0900

    fix: eslint any 타입 오류 픽스 #81

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,9 +1,33 @@
 // 전역 window 객체에 Kakao 타입 선언
+interface KakaoLinkButton {
+  title: string;
+  link: {
+    mobileWebUrl: string;
+    webUrl: string;
+  };
+}
+
+interface KakaoLinkContent {
+  title: string;
+  description: string;
+  imageUrl: string;
+  link: {
+    mobileWebUrl: string;
+    webUrl: string;
+  };
+}
+
+interface KakaoLinkParams {
+  objectType: "feed";
+  content: KakaoLinkContent;
+  buttons: KakaoLinkButton[];
+}
+
 interface Kakao {
   init: (key: string) => void;
   isInitialized: () => boolean;
   Link: {
-    sendDefault: (params: any) => void;
+    sendDefault: (params: KakaoLinkParams) => void;
   };
 }
 
