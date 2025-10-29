556377a7ff438ec8d9e569bf63aa36f0e936ab37
KimJesung <andrewkim99@naver.com>
2025-10-30 08:54:08 +0900

    feat: tag 컴포넌트 반응형 스타일링 조정 및 찜한 기사 카드 크기에 따라 분리 #133

---
+++ src/app/liked/components/CompactLikedDriverCard.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { LikedDriver } from "@/utils/hook/likes/useLikedQuery";
+import CompactLikedDriverProfile from "./CompactLikedDriverProfile";
+
+export default function CompactLikedDriverCard({
+  driver,
+  onClickAction,
+}: {
+  driver: LikedDriver;
+  onClickAction?: () => void;
+}) {
+  return (
+    <div
+      onClick={onClickAction}
+      className="flex cursor-pointer flex-col gap-3 rounded-lg border border-gray-200 bg-white p-4 shadow-sm transition hover:shadow-md"
+    >
+      {/* 상단 프로필 */}
+      <div className="flex items-center">
+        <CompactLikedDriverProfile driver={driver} />
+      </div>
+    </div>
+  );
+}
